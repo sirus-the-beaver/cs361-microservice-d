ed571015538ba13d7cc9855cc215532dd78a8084
Sirus Salari <salaris@oregonstate.edu>
2024-12-05 00:49:59 -0800

    Use wine routes in server entry

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const wineRoutes = require('./routes/wineRoutes');
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(wineRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
