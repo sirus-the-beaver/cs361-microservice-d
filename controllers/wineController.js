b1a5ab0b7e40e64fdb042180c9f5fdd937116201
Sirus Salari <salaris@oregonstate.edu>
2024-12-05 01:10:04 -0800

    Setup controller for dish recommendation based on wine

--- controllers/wineController.js
+++ controllers/wineController.js
@@ -26,3 +26,32 @@ exports.wineRecommendation = async (req, res) => {
         return res.status(500).json({ error: "Failed to fetch wine recommendations" });
     }
 };
+
+exports.dishRecommendation = async (req, res) => {
+    const { wine } = req.body;
+
+    if (!wine) {
+        return res.status(400).json({ error: "Wine is required" });
+    }
+
+    try {
+        const response = await axios.get('https://api.spoonacular.com/food/wine/dishes',
+            {
+                params: {
+                    wine,
+                    apiKey: process.env.SPOONACULAR_API_KEY
+                }
+            }
+        );
+
+        const { pairings, text } = response.data;
+
+        if (!pairings || pairings.length === 0) {
+            return res.status(404).json({ error: "No dishes found" });
+        }
+
+        return res.status(200).json({ pairings, text });
+    } catch (error) {
+        return res.status(500).json({ error: "Failed to fetch dish recommendations" });
+    }
+};
