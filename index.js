a49623788b6f77d2a8bfb9b649669f26ee94a297
Sirus Salari <salaris@oregonstate.edu>
2024-12-05 00:38:56 -0800

    Setup server entry point

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
