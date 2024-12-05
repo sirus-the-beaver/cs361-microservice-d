717208460c125615041054e2bf28e29afd421dca
Sirus Salari <salaris@oregonstate.edu>
2024-12-05 00:39:22 -0800

    Listen on port

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
